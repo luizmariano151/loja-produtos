5071c6cb2433521760ff07b8bd18b58d9b6f2005
luiz mariano <luizmarianojunior151@gmail.com>
2022-11-14 15:34:49 -0300

    docs(documentação api): adicionando a documentação da api

--- src/configuration/AppConfig.ts
+++ src/configuration/AppConfig.ts
@@ -2,7 +2,7 @@ import express, { Express, json } from "express";
 import cors from "cors";
 import UserRoutes from "../routes/UserRoutes";
 import ProductRoutes from "../routes/ProductRoutes";
-
+import { Request, Response } from "express";
 export default class App {
     private app: Express;
 
@@ -10,6 +10,7 @@ export default class App {
         this.app = express();
         this.configApp();
         this.configRoutes();
+        this.configDocumentation();
     }
 
     private configApp():void {
@@ -23,7 +24,13 @@ export default class App {
 
         this.app.use("/user", userRoutes.getRoutes());
         this.app.use("/product", productRoutes.getRoutes());
-       
+    }
+
+    public configDocumentation(): void {
+        this.app.use(express.static("documentation"));
+        this.app.use("/", (request: Request, response:Response)=>{
+            response.render("index.html");
+        });
     }
 
     public getApp(): Express {
